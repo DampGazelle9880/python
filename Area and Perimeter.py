97ef1bf10628fbeef6aef4f91addd6e784e0e1f9
DampGazelle9880 <zohanfayyaz@gmail.com>
2024-09-09 06:58:38 -0700

    new commit

---
+++ Area and Perimeter.py
@@ -0,0 +1,16 @@
+import math
+
+class Circle():
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius**2)
+    def perimeter (self):
+        return 2 * math.pi * self.radius
+obj = Circle(11)
+    
+area_cir = obj.area()
+print(f"The area of the circle is: {area_cir.2f}") 
+perimeter_cir = obj.perimeter()
+print(f"The perimeter of circle is:{perimeter_cir.2f}")
+   
