8410395bd2b824f859195032a162c7ddb5c7b248
DampGazelle9880 <zohanfayyaz@gmail.com>
2024-11-08 18:34:23 +0500

    new commit

---
+++ o(n)time.py
@@ -0,0 +1,11 @@
+def OneTime(n):
+    iteration = 0
+    for i in range(1,n+1):
+        iteration+=1
+        print("When n is",n,"iterations -", iteration)
+OneTime(7)
+OneTime(101)
+OneTime(305)
+print("\nWith every 'n' the tune taken and iterations will increase linearly")
+        
+    
